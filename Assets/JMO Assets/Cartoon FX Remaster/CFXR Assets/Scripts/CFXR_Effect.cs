4e72e4baf7c6eedcb8c8e8ae12d6377402f342b6
nguyenhuutu1106-prog <nguyenhuutu1106@gmail.com>
2026-09-15 10:28:43 +0700

    update

--- Assets/JMO Assets/Cartoon FX Remaster/CFXR Assets/Scripts/CFXR_Effect.cs
+++ Assets/JMO Assets/Cartoon FX Remaster/CFXR Assets/Scripts/CFXR_Effect.cs
@@ -28,7 +28,7 @@ namespace CartoonFX
 	public partial class CFXR_Effect : MonoBehaviour
 	{
 		// Change this value to easily tune the camera shake strength for all effects
-		const float GLOBAL_CAMERA_SHAKE_MULTIPLIER = 1.0f;
+		const float GLOBAL_CAMERA_SHAKE_MULTIPLIER = 0.3f;
 
 		static readonly int _GameObjectWorldPosition = Shader.PropertyToID("_GameObjectWorldPosition");
 
